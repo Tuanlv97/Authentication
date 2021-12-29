949f724b6c42261263cd1fae6186abdaaae3bd74
DESKTOP-G0ON5RS\DEV37 <Anhtuan20011997@gmail.com>
2021-12-30 00:13:24 +0700

    Tạo project có Identity và tìm hiểu về IdentityFrameWork

---
+++ AuthenticationSample/Entities/AplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace AuthenticationSample
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime CompanyStarteDate { get; set; }
+        [PersonalData]
+        public string Address { get; set; }
+    }
+}
